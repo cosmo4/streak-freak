1b2f60b952609c737804ce4f70bd795e1471e449
Luke Warner <cosmo4@byui.edu>
2024-10-19 22:34:12 -0600

    colors applied, header styles responsive

--- NextProjects/plums/app/page.tsx
+++ NextProjects/plums/app/page.tsx
@@ -4,19 +4,25 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-blue-50 p-8">
       {/* Header */}
-      <header className="flex items-center justify-between text-center bg-teal-100 p-6 rounded-xl shadow-lg">
-        <h1 className="text-4xl font-bold text-teal-800">Streak Freak</h1>
-        <h2 className="text-4xl font-bold text-teal-800">Habit Tracking</h2>
-        <div className="flex items-center space-x-4">
-          <SearchIcon className="w-6 h-6 text-teal-600" />
-          <UserCircleIcon className="w-8 h-8 text-teal-600" />
+      <header className="relative bg-red-100 p-6 rounded-xl shadow-lg">
+        <div className="flex justify-center">
+          <h1 className="text-3xl sm:text-4xl font-bold text-red-800 text-center">
+            Streak Freak
+            <br />
+            <h2 className='text-xl sm:text-2xl'>Habit Tracking</h2>
+          </h1>
+        </div>
+
+        <div className="absolute right-6 top-4 sm:top-10 flex flex-col-reverse sm:flex-row items-center sm:space-y-0 sm:space-x-4">
+          <SearchIcon className="w-8 h-8 text-red-600" />
+          <UserCircleIcon className="w-10 h-10 text-red-600 mb-3" />
         </div>
       </header>
 
       {/* Main Content */}
       <main className="mt-8 w-3/4 mx-auto">
         <div className="flex justify-end mb-8">
-          <button className="bg-teal-200 p-3 rounded-full shadow-md hover:bg-teal-300">
+          <button className="bg-red-200 p-3 rounded-full shadow-md hover:bg-red-300">
             <PlusIcon className="w-6 h-6 text-white" />
           </button>
         </div>
@@ -29,10 +35,10 @@ export default function Home() {
 
           {/* Existing Streak Cards */}
           {[...Array(6)].map((_, index) => (
-            <div key={index} className="bg-teal-100 rounded-xl p-6 h-40 text-center shadow-md hover:bg-teal-200">
-              <h2 className="text-teal-800 text-2xl font-bold">Streak Title</h2>
-              <p className="text-teal-600">Streak Type: Count</p>
-              <p className="text-teal-600">Total: 10</p>
+            <div key={index} className="bg-red-100 rounded-xl p-6 h-40 text-center shadow-md hover:bg-red-200">
+              <h2 className="text-red-800 text-2xl font-bold">Streak Title</h2>
+              <p className="text-red-600">Streak Type: Count</p>
+              <p className="text-red-600">Total: 10</p>
             </div>
           ))}
         </div>
