28a91adb50b09c8e2504a9183f97ce01a49b4606
BraxtonMedeiros <97545999+BraxtonMedeiros@users.noreply.github.com>
2024-12-14 16:56:34 -0700

    New Branch

--- NextProjects/plums/app/add/page.tsx
+++ NextProjects/plums/app/add/page.tsx
@@ -1,9 +1,18 @@
+'use client'
 import React from 'react';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function AddPage() {
+  const { user, error, isLoading } = useUser(); 
+  if (isLoading) return <h1>Loading...</h1>; 
+  if (error) return <h1>Error: {error.message}</h1>;
   return (
-
-      <div className="mt-8 w-3/4 mx-auto">
+      <div>
+        <div>
+        <div className="text-2xl font-bold text-purple-800 text-center mb-6"> {user ? ( <h1>Welcome, {user.name || user.nickname}</h1> ) : 
+        ( <h1>You are not logged in.</h1> )} </div>
+        </div>
+        <div className="mt-8 w-3/4 mx-auto">
         <form className="bg-purple-100 p-8 rounded-xl shadow-lg">
           <h3 className="text-2xl font-bold text-purple-800 text-center mb-6">
             Add New Streak
@@ -59,5 +68,7 @@ export default function AddPage() {
           </div>
         </form>
       </div>
+      </div>
+      
   );
 }
